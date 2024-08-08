010adb4929352cb6fb887767625593642f3e5789
Nithish <119474938+Nithish9526@users.noreply.github.com>
2024-08-08 20:28:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import LoginRegister from './Components/Assets/LoginRegister/LoginRegister'; 
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
